943dfe19644a273b3ae19261d0a6c1acae2e3559
Joaquin Diaz <joacod@gmail.com>
2023-12-22 00:19:55 +0100

    htmx config

--- handler/hello.go
+++ handler/hello.go
@@ -1,13 +1,13 @@
 package handler
 
 import (
-	"go-htmx-playground/view/hello"
+	"go-htmx-playground/api/hello"
 
 	"github.com/labstack/echo/v4"
 )
 
 type HelloHandler struct{}
 
-func (h HelloHandler) HandleHelloPage(c echo.Context) error {
-	return render(c, hello.Page())
+func (h HelloHandler) HandleHelloComponent(c echo.Context) error {
+	return render(c, hello.Component())
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./view/**/*.{html,templ}"],
+  content: ["./view/**/*.{html,templ}", "./api/**/*.{html,templ}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        opacity: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        opacity: "opacity 1s",
+      },
+    },
   },
   plugins: [],
 };
